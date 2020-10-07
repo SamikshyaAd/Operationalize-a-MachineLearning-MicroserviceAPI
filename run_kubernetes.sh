4101da3bd89795cf10c35b7b3f936eb5e0bafce8
Samikshya Adhikari <samikshyaadhikari234@gmail.com>
2020-10-06 23:29:06 -0500

    chore: Update Dockerfile, app.py, output_txt_files/docker_out.txt, output_txt_files/kubernetes_out.txt, requirements.txt, run_docker.sh, run_kubernetes.sh, upload_docker.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="samikshya234/docimg"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run microservice\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80
 
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/microservice 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="samikshya234/docimg"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag docimg $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
